313deacc2eaef0c43663cb5f43eb1bfcc7e4863e
Adil <mdadilehsan77@gmail.com>
2021-01-02 03:05:37 +0530

    pwa setup

--- public/register.js
+++ public/register.js
@@ -1,15 +1,17 @@
 // Check if Service Worker Supported in Browser
 
-if(navigator.serviceWorker){
+if (navigator.serviceWorker) {
+
 
     //Check for Load Event 
-    window.addEventListener('load',()=>{
+    window.addEventListener('load', () => {
         navigator.serviceWorker.register('cacheSite.js') // registering/specifying sw to store this js file in browser
-        .then((reg)=>{
-            console.log('Service Worker Registered'); 
-        })
-        .catch((err)=>{
-            console.log("Error : " + err);
-        })
+            .then((reg) => {
+                console.log('Service Worker Registered');
+        
+            })
+            .catch((err) => {
+                console.log("Error : " + err);
+            })
     })
 }
