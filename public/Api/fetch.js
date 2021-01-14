3f8bb8f4b05d1bef3cff2e55f230c2f92145876f
MuErThE <mohammedzabeeh@gmail.com>
2021-01-14 16:39:10 +0530

    Design changes in manga and upcoming page and adding of logo sized

--- public/Api/fetch.js
+++ public/Api/fetch.js
@@ -111,15 +111,15 @@ UpcomingAnime = () => {
         const ReleaseDate = news.Time;
         const FormatedDate = ReleaseDate.split("-")[0].split(")")[1];
 
-        Appender += ` <div class="col-12 news-card">
+        Appender += ` <div class="col-sm-12 col-md-12 col-lg-6 news-card my-4">
                 <a href="${news.redirect_link.trim()}" >
                   <div class="row">
-                    <div class="col-12 col-md-9 col-lg-9 col-sm-9">
+                    <div class="col-12">
                       <div class="news-card-title">
                         <div class="d-flex flex-row justify-content-start">
                           <h5>${news.SlNo}.</h5>
                           <img src="${news.Image_Path}" alt="" />
-                          <h4> ${news.title}</h4>
+                          <h4 class="card-display-title"> ${news.title}</h4>
                         </div>
                         <div class="news-desc">
                           <h6 class="mt-3 mb-4">
@@ -157,14 +157,13 @@ MangaNews = () => {
       const Headlines = res.data;
 
       Headlines.forEach((news) => {
-        Appender += ` <div class="col-lg-6 col-sm-6 news-card height-card px-4">
+        Appender += ` <div class="col-lg-6 col-sm-12 news-card manga-news-card height-card px-3" style="background-image:url(${news.Image_Path});">
                 <a href="${news.redirect_link.trim()}" >
                   <div class="row">
                     <div class="col-12 col-sm-12 col-lg-12">
                       <div class="news-card-title">
                         <div class="d-flex flex-row justify-content-start">
                           <h5>${news.SlNo}.</h5>
-                          <img src="${news.Image_Path}" alt="" />
                           <h4> ${news.title}</h4>
                         </div>
                         <div class="news-desc">
